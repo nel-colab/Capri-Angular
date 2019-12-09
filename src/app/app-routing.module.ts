133e8116db6a3985371f8b1b6947dd8ab5043697
nel-colab <nel.castillo95@gmail.com>
2019-12-09 01:42:00 -0300

    componente galeria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ContentComponent } from './component/main/content/content.component';
 import { NominadosComponent } from './component/main/nominados/nominados.component';
+import { GaleriaComponent } from './component/main/galeria/galeria.component';
 
 const router: Routes = [
   
 
   { path: 'home', component: ContentComponent },
+  { path: 'galeria', component: GaleriaComponent },
   { path: 'nominados', component: NominadosComponent },
   { path: '', component: ContentComponent },
   { path: '**', component: ContentComponent }
